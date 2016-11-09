e3deeab7ad67a40d30eceff1b2888268bcfc0170
Tony Edgin <tedgin@iplantcollaborative.org>
2016-11-09 13:36:18 -0700

    added test for minimal user creation

--- tests/verify.sh
+++ tests/verify.sh
@@ -1,3 +1,15 @@
 #! /bin/bash
 
-rabbitmqctl list_vhosts | grep --quiet '/vhost'
+set -e
+
+if ! (rabbitmqctl list_vhosts | grep --quiet '/vhost')
+then
+  printf 'failed to set vhost /vhost\n'
+fi
+
+
+if ! (rabbitmqctl list_permissions -p /vhost \
+        | grep --quiet --regexp '^admin[[:space:]]*\^\$[[:space:]]*\^\$[[:space:]]*\^\$')
+then
+  printf 'failed to set user admin correctly\n'
+fi
