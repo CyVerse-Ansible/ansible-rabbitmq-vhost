ccf66e4d01992651b0e3dc9fc3d1637c64743b74
Tony Edgin <tedgin@iplantcollaborative.org>
2016-11-10 09:01:45 -0700

    added test for user perm and tags

--- tests/manual_test.sh
+++ tests/manual_test.sh
@@ -7,8 +7,10 @@ readonly TEST_DIR="$ROLE_DIR"/tests
 
 
 # Start the container
+printf 'Starting docker container\n'
 docker run --detach --interactive --tty --name=test_dummy --volume="$PWD"/..:"$ROLE_DIR":rw \
-           cyverse/ansible-test:latest-"$distribution"-"$version" bash
+           cyverse/ansible-test:latest-"$distribution"-"$version" bash \
+  > /dev/null
 
 if [ "$?" -ne 0 ]
 then
@@ -19,12 +21,14 @@ fi
 # Install required software
 if [ "$?" -eq 0 ]
 then
-  docker exec test_dummy "$TEST_DIR"/bootstrap.sh
+  printf 'Installing required software\n'
+  docker exec test_dummy "$TEST_DIR"/bootstrap.sh > /dev/null
 fi
   
 # Check syntax of role
 if [ "$?" -eq 0 ]
 then
+  printf 'Checking syntax\n'
   docker exec test_dummy \
     ansible-playbook --syntax-check --inventory-file="$TEST_DIR"/inventory "$TEST_DIR"/test.yml
 fi
@@ -32,6 +36,7 @@ fi
 # Run configuration test
 if [ "$?" -eq 0 ]
 then
+  printf 'Running configuration playbook\n'
   docker exec test_dummy \
     ansible-playbook --connection=local --inventory-file="$TEST_DIR"/inventory "$TEST_DIR"/test.yml
 fi
@@ -39,19 +44,22 @@ fi
 # Verify configuration
 if [ "$?" -eq 0 ]
 then
+  printf 'Verifying configuration\n'
   docker exec test_dummy "$TEST_DIR"/verify.sh
 fi
 
 # Idempotentence test
 if [ "$?" -eq 0 ]
 then
+  printf 'Checking idempotence of role\n'
   docker exec test_dummy \
         ansible-playbook --connection=local --inventory-file="$TEST_DIR"/inventory \
                          "$TEST_DIR"/test.yml \
     | grep --quiet --regexp='changed=0.*failed=0' \
-      && echo 'Idempotence test: pass' \
-      || echo 'Idempotence test: fail'
+      && printf 'Idempotence test: pass\n' \
+      || printf 'Idempotence test: fail\n'
 fi
 
-docker stop test_dummy
-docker rm test_dummy
+printf 'Cleaning up\n'
+docker stop test_dummy > /dev/null
+docker rm test_dummy > /dev/null
--- tests/verify.sh
+++ tests/verify.sh
@@ -7,9 +7,13 @@ then
   printf 'failed to set vhost /vhost\n'
 fi
 
+if ! (rabbitmqctl list_users | grep --quiet --regexp '^admin[[:space:]]*\[administrator\]')
+then
+  printf 'failed to set user admin correctly\n'
+fi
 
 if ! (rabbitmqctl list_permissions -p /vhost \
-        | grep --quiet --regexp '^admin[[:space:]]*\^\$[[:space:]]*\^\$[[:space:]]*\^\$')
+        | grep --quiet --regexp '^admin[[:space:]]*\.\*[[:space:]]*\.\*[[:space:]]*\.\*')
 then
   printf 'failed to set user admin correctly\n'
 fi
