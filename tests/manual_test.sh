fbdc1c9a0b811e843d603899023d5806e3a4c8af
Tony Edgin <tedgin@iplantcollaborative.org>
2016-11-10 11:06:09 -0700

    moved tests into ansible

--- tests/manual_test.sh
+++ tests/manual_test.sh
@@ -7,59 +7,63 @@ readonly TEST_DIR="$ROLE_DIR"/tests
 
 
 # Start the container
-printf 'Starting docker container\n'
+printf '\n--> Starting docker container\n'
 docker run --detach --interactive --tty --name=test_dummy --volume="$PWD"/..:"$ROLE_DIR":rw \
            cyverse/ansible-test:latest-"$distribution"-"$version" bash \
   > /dev/null
-
-if [ "$?" -ne 0 ]
-then
-  exit 1
-fi
-
+err="$?"
 
 # Install required software
-if [ "$?" -eq 0 ]
+if [ "$err" -eq 0 ]
 then
-  printf 'Installing required software\n'
+  printf '\n--> Installing required software\n'
   docker exec test_dummy "$TEST_DIR"/bootstrap.sh > /dev/null
+  err="$?"
 fi
-  
+
 # Check syntax of role
-if [ "$?" -eq 0 ]
+if [ "$err" -eq 0 ]
 then
-  printf 'Checking syntax\n'
+  printf '\n--> Checking syntax\n'
   docker exec test_dummy \
     ansible-playbook --syntax-check --inventory-file="$TEST_DIR"/inventory "$TEST_DIR"/test.yml
+  err="$?"
 fi
 
 # Run configuration test
-if [ "$?" -eq 0 ]
+if [ "$err" -eq 0 ]
 then
-  printf 'Running configuration playbook\n'
+  printf '\n--> Running configuration playbook\n'
   docker exec test_dummy \
     ansible-playbook --connection=local --inventory-file="$TEST_DIR"/inventory "$TEST_DIR"/test.yml
-fi
-
-# Verify configuration
-if [ "$?" -eq 0 ]
-then
-  printf 'Verifying configuration\n'
-  docker exec test_dummy "$TEST_DIR"/verify.sh
+  err="$?"
 fi
 
 # Idempotentence test
-if [ "$?" -eq 0 ]
+if [ "$err" -eq 0 ]
 then
-  printf 'Checking idempotence of role\n'
+  printf '\n--> Checking idempotence of role\n'
   docker exec test_dummy \
-        ansible-playbook --connection=local --inventory-file="$TEST_DIR"/inventory \
-                         "$TEST_DIR"/test.yml \
+      ansible-playbook --connection=local --inventory-file="$TEST_DIR"/inventory --skip-tags=test \
+                       "$TEST_DIR"/test.yml \
     | grep --quiet --regexp='changed=0.*failed=0' \
       && printf 'Idempotence test: pass\n' \
       || printf 'Idempotence test: fail\n'
 fi
 
-printf 'Cleaning up\n'
+printf '\n--> Cleaning up\n'
 docker stop test_dummy > /dev/null
+e2="$?"
+if [ "$e2" -ne 0 ]
+then
+  err="$e2"
+fi
+
 docker rm test_dummy > /dev/null
+e2="$?"
+if [ "$e2" -ne 0 ]
+then
+  err="$e2"
+fi
+
+exit "$err"
